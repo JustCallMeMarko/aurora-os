e8a7b405a02d0b958d07964ba8556f9e178e4a33
JustCallMeMarko <cantimbuhanmark.stnick@gmail.com>
2025-08-21 20:41:17 +0800

    fixed vercel not redirecting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Login from "./pages/Login"
-import Home from "./pages/Home"
-import Layout from "./layout/Layout"
-import Chatbot from "./pages/Chatbot"
-import Spotify from "./pages/Spotify"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Layout from "./layout/Layout";
+import Chatbot from "./pages/Chatbot";
+import Spotify from "./pages/Spotify";
 
 function App() {
-
   return (
-   <BrowserRouter>
-   <Routes>
-     <Route path="/" element={<Login />} />
-     <Route element={<Layout />}>
-       <Route path="/home" element={<Home />} />
-       <Route path="/ai" element={<Chatbot />} />
-       <Route path="/spotify" element={<Spotify />} />
-     </Route>
-   </Routes>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/ai" element={<Chatbot />} />
+          <Route path="/spotify" element={<Spotify />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
