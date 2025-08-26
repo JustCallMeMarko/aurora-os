e228663da1db6187d6d16a1293539bf300a8891b
JustCallMeMarko <cantimbuhanmark.stnick@gmail.com>
2025-08-26 13:43:49 +0800

    changed loading

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -5,8 +5,20 @@ function Loading() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-center items-center w-screen h-screen" onClick={() => navigate("/login")}>
-      <video src={loading} autoPlay muted playsInline className="block object-cover w-full md:w-[40%] h-[40%]" onClick={() => navigate("/login")}/>
+    <div
+      className="flex justify-center items-center w-screen h-screen"
+      onClick={() => navigate("/login")}
+    >
+      <video
+        src={loading}
+        autoPlay
+        muted
+        playsInline
+        preload="auto"
+        poster="/Loading.mp4"
+        className="block object-cover w-full md:w-[40%] h-[40%]"
+        onClick={() => navigate("/login")}
+      />
     </div>
   );
 }
